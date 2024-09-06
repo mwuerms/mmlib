7d77d9f0075bdb8972d91d3ef0550a13042afb53
Martin Egli <m_wuerms@gmx.net>
2024-09-06 22:55:20 +0200

    renamed string.c/.h -> str_buf.c/.h
    also added function str_buf_append_uint8(), str_buf_append_int8(),
    str_buf_append_uint16(), str_buf_append_int16(),
    str_buf_append_uint32(), str_buf_append_int32(), str_buf_append_float()
    also added tests: all successfull

--- str_buf.h
+++ str_buf.h
@@ -1,23 +1,23 @@
 /**
  * Martin Egli
  * 2023-05-03
- * mmlib: sting functions
+ * mmlib: str_buf string buffer functions
  */
 
-#ifndef _MM_STRING_H_
-#define _MM_STRING_H_
+#ifndef _MM_str_buf_H_
+#define _MM_str_buf_H_
 
 #include <stdint.h>
 
 #define STRING_TERMINATION '\0'
 
 /**
- * clear the string, set STRING_TERMINATION to head (position = 0) ot the string
+ * clear the string, set str_buf_TERMINATION to head (position = 0) ot the string
  * @param   str         string to find the end in
  * @param   str_size    size of the string
  * @return  pos = 0: valid position
  */
-uint16_t string_clear(char *str, uint16_t str_size);
+uint16_t str_buf_clear(char *str, uint16_t str_size);
 
 /**
  * find the end position of a given string, '\0'-terminated
@@ -26,7 +26,18 @@ uint16_t string_clear(char *str, uint16_t str_size);
  * @return  pos = 0 ... x: valid position, found the end before str_size, 
  *          pos = str_size: could not find the end, pos invalid
  */
-uint16_t string_find_end_pos(char *str, uint16_t str_size);
+uint16_t str_buf_find_end_pos(char *str, uint16_t str_size);
+
+/** 
+ * append a single char c to the string str
+ * @param   str         destination string to append to, this string will be changed
+ * @param   str_size    size of the destination string
+ * @param   c           char to append
+ * @return  pos, end position in string
+ *          = 0 ... x: valid position
+ *          = str_size: appended as much as possible, but reached the end of destination string
+ */
+uint16_t str_buf_append_char(char *str, uint16_t str_size, char c);
 
 /** 
  * append the string str_append to the string str
@@ -37,7 +48,7 @@ uint16_t string_find_end_pos(char *str, uint16_t str_size);
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_append(char *str, uint16_t str_size, char *str_append);
+uint16_t str_buf_append_string(char *str, uint16_t str_size, char *str_append);
 
 /** 
  * copy a string to de destination string, give position where to copy and copy string size
@@ -48,7 +59,7 @@ uint16_t string_append(char *str, uint16_t str_size, char *str_append);
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_copy_pos_size(char *str, uint16_t str_size, char *str_copy, uint16_t copy_to_pos, uint16_t copy_size);
+uint16_t str_buf_copy_pos_size(char *str, uint16_t str_size, char *str_copy, uint16_t copy_to_pos, uint16_t copy_size);
 
 /**
  * delete a given number of chars from a given position of the string
@@ -60,7 +71,7 @@ uint16_t string_copy_pos_size(char *str, uint16_t str_size, char *str_copy, uint
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_delete_from_position(char *str, uint16_t str_size, uint16_t del_pos, uint16_t nb_chars);
+uint16_t str_buf_delete_from_position(char *str, uint16_t str_size, uint16_t del_pos, uint16_t nb_chars);
 
 /**
  * delete a given number of chars from the head (position = 0) of the string
@@ -71,7 +82,7 @@ uint16_t string_delete_from_position(char *str, uint16_t str_size, uint16_t del_
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_delete_from_head(char *str, uint16_t str_size, uint16_t nb_chars);
+uint16_t str_buf_delete_from_head(char *str, uint16_t str_size, uint16_t nb_chars);
 
 /**
  * delete a given number of chars from the tail (end) of the string
@@ -82,7 +93,7 @@ uint16_t string_delete_from_head(char *str, uint16_t str_size, uint16_t nb_chars
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_delete_from_tail(char *str, uint16_t str_size, uint16_t nb_chars);
+uint16_t str_buf_delete_from_tail(char *str, uint16_t str_size, uint16_t nb_chars);
 
 /**
  * append a number in uint8_t as chars to the destination string
@@ -93,41 +104,41 @@ uint16_t string_delete_from_tail(char *str, uint16_t str_size, uint16_t nb_chars
  *          = 0 ... x: valid position
  *          = str_size: appended as much as possible, but reached the end of destination string
  */
-uint16_t string_append_uint8(char *str, uint16_t str_size, uint8_t num);
+uint16_t str_buf_append_uint8(char *str, uint16_t str_size, uint8_t num);
 
 /**
  * append a number in int8_t as chars with sign (-='-', +=' ') to the destination string
  */
-uint16_t string_append_int8(char *str, uint16_t str_size, int8_t num);
+uint16_t str_buf_append_int8(char *str, uint16_t str_size, int8_t num);
 
 /**
  * append a number in uint16_t as chars to the destination string
  */
-uint16_t string_append_uint16(char *str, uint16_t str_size, uint16_t num);
+uint16_t str_buf_append_uint16(char *str, uint16_t str_size, uint16_t num);
 
 /**
  * append a number in int16_t as chars with sign (-='-', +=' ') to the destination string
  */
-uint16_t string_append_int16(char *str, uint16_t str_size, int16_t num);
+uint16_t str_buf_append_int16(char *str, uint16_t str_size, int16_t num);
 
 /**
  * append a number in uint32_t as chars to the destination string
  */
-uint16_t string_append_uint32(char *str, uint16_t str_size, uint32_t num);
+uint16_t str_buf_append_uint32(char *str, uint16_t str_size, uint32_t num);
 
 /**
  * append a number in int32_t as chars with sign (-='-', +=' ') to the destination string
  */
-uint16_t string_append_int32(char *str, uint16_t str_size, int32_t num);
+uint16_t str_buf_append_int32(char *str, uint16_t str_size, int32_t num);
 
 /**
  * append a number in float as chars with sign (-='-', +=' ') to the destination string
  * post point presision = number of chars after '.'
  */
-uint16_t string_append_float(char *str, uint16_t str_size, float num, uint16_t post_presision);
+uint16_t str_buf_append_float(char *str, uint16_t str_size, float num, uint16_t post_presision);
 
 /**
  * 
  */
 
-#endif // _MM_STRING_H_
+#endif // _MM_str_buf_H_
